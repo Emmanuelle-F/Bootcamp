4a09e73e462ef33f97a7f2bb570868074ab0690f
Emma <emmafrapppier0403@gmail.com>
2025-06-28 08:59:50 +0400

    add

---
+++ week1/day1/exercise1.py
@@ -0,0 +1,7 @@
+name = "Alice"
+age = 30
+city = "New York"
+
+print (f"Hello, {name}! You are {age} years old and live in {city}") # Using f-strings
+
+print ("Hello, {}! You are {} years old and live in {}".format(name, age, city)) # Using str.format
---
+++ week1/day1/exercise2.py
@@ -0,0 +1,5 @@
+age = int(input("Please enter your age: "))
+
+numberOfYears = 100 - age
+
+print(f"You will turn 100 in {numberOfYears} years")
