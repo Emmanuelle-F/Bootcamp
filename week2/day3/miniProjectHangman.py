cfb7e36fd0933749bf65da7385684a90791374ad
Emma <emmafrapppier0403@gmail.com>
2025-07-15 21:08:27 +0400

    update project

--- week2/day3/miniProjectHangman.py
+++ week2/day3/miniProjectHangman.py
@@ -3,15 +3,79 @@ import random
 # List of possible words
 wordList = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
 
-gallows  = [
-    "Head",
-    "Head, Body",
-    "Head, Body, Left Arm",
-    "Head, Body, Left Arm, Right Arm",
-    "Head, Body, Left Arm, Right Arm, Left Leg",
-    "Head, Body, Left Arm, Right Arm, Left Leg, Right Leg"
+# Hangman stages for each wrong guess
+hangman_stages = [
+    """
+--------------
+|            |
+|            
+|            
+|            
+|            
+|
+--------------
+""",
+    """
+--------------
+|            |
+|            O
+|            
+|            
+|            
+|
+--------------
+""",
+    """
+--------------
+|            |
+|            O
+|            |
+|            
+|            
+|
+--------------
+""",
+    """
+--------------
+|            |
+|            O
+|           /|
+|            
+|            
+|
+--------------
+""",
+    """
+--------------
+|            |
+|            O
+|           /|\\
+|            
+|            
+|
+--------------
+""",
+    """
+--------------
+|            |
+|            O
+|           /|\\
+|           / 
+|            
+|
+--------------
+""",
+    """
+--------------
+|            |
+|            O
+|           /|\\
+|           / \\
+|            
+|
+--------------
+"""
 ]
-
 
 # Choose a word randomly from the wordList
 def choose_word():
@@ -21,28 +85,24 @@ def choose_word():
 # If guess letter is right, display the letter, else display *
 def display_word(word, guessed_letters):
     result = ""
-
     for letter in word:
         if letter in guessed_letters:
             result += letter + " "
         else:
             result += "* "
-
     return result.strip()
 
-
 def play_hangman():
-
     word = choose_word()
-
-    guessed_letters = set()  # use a set to avoid duplicate letters
+    guessed_letters = set()
     wrong_guesses = 0
-    max_wrong = len(gallows ) 
+    max_wrong = len(hangman_stages) - 1
 
     print("Welcome to Hangman!")
     print(display_word(word, guessed_letters))
 
     while wrong_guesses < max_wrong:
+        print(hangman_stages[wrong_guesses])
         guess = input("Guess a letter: ").lower()
 
         if not guess.isalpha() or len(guess) != 1:
@@ -60,7 +120,6 @@ def play_hangman():
         else:
             wrong_guesses += 1
             print("Wrong!")
-            print(f"Parts on gallows: {gallows [wrong_guesses-1]}")
 
         current_display = display_word(word, guessed_letters)
         print(current_display)
@@ -69,8 +128,8 @@ def play_hangman():
             print("Congratulations! You guessed the word!")
             break
     else:
+        print(hangman_stages[wrong_guesses])
         print(f"You lost! The word was: {word}")
 
+play_hangman()
 
-if __name__ == "__main__":
-    play_hangman()
